6375a678e0fa5ee865b41c7ae68fd7eb8a8e03e0
leegayeong <leegy0419@gmail.com>
2022-06-12 23:55:07 +0900

    콜라츠 추측

---
+++ Programmers/src/lv1/평균구하기.java
@@ -0,0 +1,25 @@
+package lv1;
+
+import java.util.Scanner;
+
+class Solution5{
+	public double solution(int[] arr) {
+		double answer = 0;
+		int sum = 0;
+		for(int i : arr) 
+			sum += i;
+		answer = sum/arr.length;
+		return answer;
+	}
+}
+public class Æò±Õ±¸ÇÏ±â {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int[] arr;
+		
+		
+	}
+
+}
