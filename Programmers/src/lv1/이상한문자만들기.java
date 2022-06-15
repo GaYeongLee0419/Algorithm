26d04ef3ef3c06f6235342c8a0965da999ecac04
leegayeong <leegy0419@gmail.com>
2022-06-15 23:49:16 +0900

    이상한 문자 만들기

---
+++ Programmers/src/lv1/이상한문자만들기.java
@@ -0,0 +1,40 @@
+package lv1;
+
+class Solution14{
+	public String solution(String s) {
+		String answer = "";
+		int cnt = 0;
+		for(int i=0; i<s.length(); i++) {
+			String str = s.substring(i, i+1);
+
+			System.out.printf("cnt : %d , %s\n", cnt, str);
+			if(str.equals(" ")) {
+				cnt = 0;
+				answer += str;
+				continue;
+			}
+			if(cnt%2 == 0) {
+				answer += str.toUpperCase();
+				cnt++;
+
+			}
+			else {
+				answer += str.toLowerCase();
+				cnt++;
+			}
+		}
+		return answer;
+	}
+}
+public class 이상한문자만들기 {
+
+	public static void main(String[] args) {
+		String s = "this is me gayeong lee";
+		
+		Solution14 sol = new Solution14();
+		
+		System.out.println(sol.solution(s));
+
+	}
+
+}
