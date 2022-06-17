056a69ef6f60f993c35bc6eeb6a7a73f9418ab8e
leegayeong <leegy0419@gmail.com>
2022-06-17 23:06:24 +0900

    두 개 뽑아서 더하기 수정

--- Programmers/src/lv1/두개뽑아서더하기.java
+++ Programmers/src/lv1/두개뽑아서더하기.java
@@ -1,5 +1,26 @@
 package lv1;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class Solution16{
+	public int[] solution(int[] numbers) {
+		ArrayList<Integer> list = new ArrayList<>();
+		
+		for(int i=0; i< numbers.length; i++) {
+			for(int j=i; j < numbers.length; j++) {
+				if(list.contains(numbers[i] + numbers[j]))
+					continue;
+				list.add(numbers[i] + numbers[j]);
+			}
+		}
+		int[] answer = list.stream()
+						.mapToInt(Integer::intValue)
+						.toArray();
+		Arrays.sort(answer);
+		return answer;
+		}
+	}
 public class 두개뽑아서더하기 {
 
 	public static void main(String[] args) {
