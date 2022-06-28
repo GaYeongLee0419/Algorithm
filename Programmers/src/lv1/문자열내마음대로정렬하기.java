dd714c8ba18c458ee1ddfd11f94d3feeab32d1ec
GaYeongLee0419 <68856259+GaYeongLee0419@users.noreply.github.com>
2022-06-28 23:28:38 +0900

    Update 문자열내마음대로정렬하기.java

--- Programmers/src/lv1/문자열내마음대로정렬하기.java
+++ Programmers/src/lv1/문자열내마음대로정렬하기.java
@@ -1,7 +1,17 @@
-class Solution30{
-    public String solution(String s){
-        String answer = "";
-        if(s.length()%2 == 0) answer = s.substring()
+class Solution25{
+    public String[] solution(String[] strings, int n){
+        String[] answer = new String[strings.length];
+        ArrayList<String> arr = new ArrayList<>();
+
+        for(int i=0; i<answer.length; i++)
+            arr.add(strings[i].substring(n,n+1)+strings[i]);
+
+        Collections.sort(arr);
+
+        for(int i=0; i< strings.length; i++){
+            answer[i] = arr.get(i).substring(1,arr.get(i).length());
+        }
+
         return answer;
     }
 }
