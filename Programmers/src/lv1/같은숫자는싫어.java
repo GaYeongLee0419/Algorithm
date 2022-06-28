1adf0b6dec39593d3a077a2f2b05d3cf33860bbe
GaYeongLee0419 <68856259+GaYeongLee0419@users.noreply.github.com>
2022-06-28 23:31:41 +0900

    Update 같은숫자는싫어.java

--- Programmers/src/lv1/같은숫자는싫어.java
+++ Programmers/src/lv1/같은숫자는싫어.java
@@ -1,19 +1,24 @@
 class Solution29{
-    public int solution(int n){
-        int answer = 0;
-        for(int i=1; i<n; i++){
-            if(n%i == 1) {
-                answer = i;
-                break;
+    public int[] solution(int[] arr){
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(arr[0]);
+        for(int i=0; i<arr.length; i++){
+            if(list.get(list.size()-1) != arr[i]) {
+                list.add(arr[i]);
             }
+        }
+        int[] answer = new int[list.size()];
+        for(int i=0; i< answer.length; i++){
+            answer[i] = list.get(i).intValue();
         }
         return answer;
     }
 }
-public class 나머지가1이되는수찾기 {
+public class 같은숫자는싫어 {
     public static void main(String[] args) {
-        int n = 12;
+        int[] arr = {1,1,3,3,0,1,1};
         Solution29 sol = new Solution29();
-        System.out.println(sol.solution(n));
+        int[] answer = sol.solution(arr);
+        System.out.println(Arrays.toString(answer));
     }
 }
