247ecb224676159ff0b06016c749226118dc8a0f
GaYeongLee0419 <68856259+GaYeongLee0419@users.noreply.github.com>
2023-01-30 23:38:28 +0900

    Update 소수만들기.java
    문제해결방법 고민하기

--- Programmers/src/lv1/소수만들기.java
+++ Programmers/src/lv1/소수만들기.java
@@ -1,3 +1,10 @@
+// 1. List 원소 3개 뽑아서 더하기
+//  - for문을 3번 반복해서 해결?
+//  - for문을 반복할 때 중복이 생길 수 있다.
+
+// 2. 소수 판별하기
+//  - x를 2로 나누고 2~(x/2) 까지의 정수로 나눠서 전부 나머지가 0이 아니면 소수
+
 class Solution {
    public int solution(int[] nums) {
       int answer = 0;
